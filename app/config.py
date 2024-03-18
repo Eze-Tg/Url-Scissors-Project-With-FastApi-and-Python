a006ef1401d92473dc3943f75ea1da90e843fb57
i-teegee <israelthankgod@gmail.com>
2024-03-18 11:25:51 +0100

    changed from local Postgres to Render Postgres

--- app/config.py
+++ app/config.py
@@ -3,11 +3,18 @@
 from functools import lru_cache
 
 from pydantic_settings import BaseSettings
+from dotenv import find_dotenv, load_dotenv
+import os
+
+dotenv_path = find_dotenv()
+
+load_dotenv(dotenv_path)
+DB_URL = os.getenv('DB_URL')
 
 class Settings(BaseSettings):
     env_name: str = "Development"
     base_url: str = "http://localhost:8000"
-    db_url: str = 'postgresql://postgres:password@localhost/url_scissors_db'
+    db_url: str = DB_URL
 
     class Config:
         env_file = ".env"
