bbfa522f0d9eafce211ffc169467697f57de307a
i-teegee <israelthankgod@gmail.com>
2024-03-20 22:55:30 +0100

    Succesfully generates Qrcodes and renders it on screen

--- app/static/url_short/js/script.js
+++ app/static/url_short/js/script.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const urlInput = document.getElementById('longUrlInput');
     const shortUrlDisplay = document.getElementById('shortUrl');
+    const new_short_url = document.getElementById('new_short_url');
     const qrCodeContainer = document.getElementById('qrCodeContainer');
     const shortUrlContainer = document.getElementById('shortUrlInput');
 
@@ -18,8 +19,8 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(response => response.json())
         .then(data => {
-            // const new_short_url = data.url
             // Display short URL
+            new_short_url.textContent = data.url
             shortUrlDisplay.textContent = `Shortened URL: ${data.url}`;
 
         })
@@ -28,27 +29,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function QrUrl() {
         // Try to QR code the short URL
-    const shortUrl = shortUrlDisplay.textContent; // Get the shortened URL from the display
+        const shortUrl = new_short_url.textContent; // Get the shortened URL from the display
 
-    fetch('/url_to_qr', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ target_url: shortUrl }) 
-    })
-    .then(response => response.blob()) // Convert response to Blob object
-    .then(blob => {
-        // Create object URL for the Blob
-        const imageUrl = URL.createObjectURL(blob);
+        fetch('/url_to_qr', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ target_url: shortUrl }) 
+        })
+        .then(response => response.blob()) // Convert response to Blob object
+        .then(blob => {
+            // Create object URL for the Blob
+            const imageUrl = URL.createObjectURL(blob);
 
-        // Display QR code
-        qrCodeContainer.innerHTML = `<img src="${imageUrl}" alt="QR Code">`;
+            // Display QR code
+            qrCodeContainer.innerHTML = `<img src="${imageUrl}" alt="QR Code">`;
 
-        // Optionally, you can revoke the object URL to free up resources
-        // URL.revokeObjectURL(imageUrl);
-    })
-    .catch(error => console.error('Error:', error));}
+            // Optionally, you can revoke the object URL to free up resources
+            // URL.revokeObjectURL(imageUrl);
+        })
+        .catch(error => console.error('Error:', error));}
 
     // Attach click event listener to the button
     const shortenUrlButton = document.getElementById('shortenButton');
