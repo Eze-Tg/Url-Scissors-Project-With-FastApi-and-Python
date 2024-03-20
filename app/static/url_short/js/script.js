7456ffb4d0816b094209030fea17d7c26d4703e0
i-teegee <israelthankgod@gmail.com>
2024-03-20 22:05:53 +0100

    Trying to deliver qrcode in frontend

--- app/static/url_short/js/script.js
+++ app/static/url_short/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const urlInput = document.getElementById('longUrlInput');
     const shortUrlDisplay = document.getElementById('shortUrl');
     const qrCodeContainer = document.getElementById('qrCodeContainer');
+    const shortUrlContainer = document.getElementById('shortUrlInput');
 
     function shortenUrl() {
         const longUrl = urlInput.value.trim();
@@ -17,17 +18,43 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(response => response.json())
         .then(data => {
+            // const new_short_url = data.url
             // Display short URL
             shortUrlDisplay.textContent = `Shortened URL: ${data.url}`;
 
-            // Display QR code
-            qrCodeContainer.innerHTML = `<img src="data:image/png;base64,${data.qr_code}" alt="QR Code">`;
         })
         .catch(error => console.error('Error:', error));
     }
 
+    function QrUrl() {
+        // Try to QR code the short URL
+    const shortUrl = shortUrlDisplay.textContent; // Get the shortened URL from the display
+
+    fetch('/url_to_qr', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ target_url: shortUrl }) 
+    })
+    .then(response => response.blob()) // Convert response to Blob object
+    .then(blob => {
+        // Create object URL for the Blob
+        const imageUrl = URL.createObjectURL(blob);
+
+        // Display QR code
+        qrCodeContainer.innerHTML = `<img src="${imageUrl}" alt="QR Code">`;
+
+        // Optionally, you can revoke the object URL to free up resources
+        // URL.revokeObjectURL(imageUrl);
+    })
+    .catch(error => console.error('Error:', error));}
+
     // Attach click event listener to the button
     const shortenUrlButton = document.getElementById('shortenButton');
     shortenUrlButton.addEventListener('click', shortenUrl);
+
+    const QrcodeButton = document.getElementById('qrcodeButton');
+    QrcodeButton.addEventListener('click', QrUrl);
 });
 
